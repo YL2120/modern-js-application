ec4a7038e9d2121bff0d6a52fb74016c55584d73
Yassine Layachi <yassinelayachi@outlook.com>
2021-08-13 15:44:32 +0200

    post characters step finished

--- create.js
+++ create.js
@@ -1,48 +1,72 @@
 function previewFile() {
-    const preview = document.querySelector('img');
-    const file = document.querySelector('input[type=file]').files[0];
-    const reader = new FileReader();
-    reader.addEventListener("load", function () {
+  const preview = document.querySelector("img");
+  const file = document.querySelector("input[type=file]").files[0];
+  const reader = new FileReader();
+  reader.addEventListener(
+    "load",
+    function () {
       // convert image file to base64 string
       preview.src = reader.result;
-    }, false);
-  
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-};
+    },
+    false
+  );
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
 
 document.getElementById("save").addEventListener("click", () => {
-    const reader = new FileReader();
-    reader.readAsDataURL(document.querySelector('input[type="file"]').files[0]);
-    reader.onload = () => storeResults(reader.result);
+  const reader = new FileReader();
+  reader.readAsDataURL(document.querySelector('input[type="file"]').files[0]);
+  reader.onload = () => storeResults(reader.result);
 
-    // callback function
-    async function storeResults(result) {
+  // callback function
+  async function storeResults(result) {
     let dataURL = result;
-    let inputs = Array.from(document.querySelectorAll(".textInput"));
-    inputs.push(dataURL);
+    console.log(dataURL); //1
+    let numIndex = dataURL.indexOf("/9j");
+    console.log(numIndex); //2
+    let sliceData = dataURL.slice(numIndex);
+    console.log(sliceData); //3 
+    
+    
+    let inputs = Array.from(document.querySelectorAll(".textInput"));  
     let values = inputs.map(({ value }) => {
-    return value.trim(); // Removes blank spaces before and after string input
+      return value.trim(); // Removes blank spaces before and after string input
     });
 
     if (values.some((value) => value === "")) {
-    alert("Please fill in all fields");
-    return;
-    };
+      alert("Please fill in all fields");
+      return;
+    }
+
+    values.push(sliceData);
+    console.log(values);
 
     let [name, shortDescription, description, image] = values;
-    // let id = null;
+    //let id = null;
 
-    const postData = await fetch("https://character-database.becode.xyz/characters", {
+    console.log([name, shortDescription, description, image]); //4
+
+    const postData = await fetch(
+      "https://character-database.becode.xyz/characters",
+      {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+          
         },
-        body: JSON.stringify({name, shortDescription, description, image }),
-        });
-        console.log(postData);
-        console.log(postData.json());
-    };
-
-})
+        body: JSON.stringify({
+          name,
+          shortDescription,
+          description,
+          image,
+        }),
+      }
+    );
+    console.log(postData);
+    console.log(postData.json());
+  }
+});
