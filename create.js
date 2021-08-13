d51cacacb5b2c7e7dd8b955c3a81346e9a50e233
Baysaaaa <bayas.chuluun@gmail.com>
2021-08-13 10:53:03 +0200

    js should work, need to set up server connection

--- create.js
+++ create.js
@@ -1,6 +1,3 @@
-// Retrieving image File from user's input in the form
-// const inputFile = document.querySelector('input[type=file]');
-
 function previewFile() {
     const preview = document.querySelector('img');
     const file = document.querySelector('input[type=file]').files[0];
@@ -15,79 +12,37 @@ function previewFile() {
     }
 };
 
-const imgtodata=()=>{
-const reader = new FileReader();
-reader.readAsDataURL(document.querySelector('input[type="file"]').files[0]);
-reader.onload = () => storeResults(reader.result);
+document.getElementById("save").addEventListener("click", () => {
+    const reader = new FileReader();
+    reader.readAsDataURL(document.querySelector('input[type="file"]').files[0]);
+    reader.onload = () => storeResults(reader.result);
 
-// callback function
-function storeResults(result) {
-  let dataURL = result;
-  console.log(dataURL);
+    // callback function
+    async function storeResults(result) {
+    let dataURL = result;
+    let inputs = Array.from(document.querySelectorAll(".textInput"));
+    inputs.push(dataURL);
+    let values = inputs.map(({ value }) => {
+    return value.trim(); // Removes blank spaces before and after string input
+    });
 
-}
+    if (values.some((value) => value === "")) {
+    alert("Please fill in all fields");
+    return;
+    };
 
+    let [name, shortDescription, description, image] = values;
+    // let id = null;
 
+    const postData = await fetch("https://character-database.becode.xyz/characters", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name, shortDescription, description, image }),
+        });
+        console.log(postData);
+        console.log(postData.json());
+    };
 
-
-
-}
-
-
-
-
-// const inputs = Array.from(document.querySelectorAll("input"));
-// console.log(inputs);
-
-// // Create a Promise to convert 'File' to 'string Base64'
-// const toBase64 = (file) =>{
-//     const reader = new FileReader();
-//     return new Promise ((resolve,reject) => {
-//         reader.onerror = () => {
-//             reader.abort();
-//             reject(new DOMException("Problem parsing input file."))
-//         };
-//         reader.onload = () => {
-//             resolve(reader.result);
-//         };
-//         reader.readAsDataURL(file);
-//     });
-// };
-
-// toBase64().then((result)=>console.log(result))
-
-// const imageToDataURL = async () =>{
-//     const inputFile = document.querySelector('input[type=file]').files[0];
-//     // Asynchronous function awaits for the Promise to resolve and return the proper data URL
-//     dataURL= await toBase64(inputFile);
-// };
-// console.log(dataURL);
-
-//     let values = inputs.map(({ value }) => {
-//       return value.trim();
-//     });
-
-//     if (values.some((value) => value === "")) {
-//       alert("Please fill in all fields");
-//       return;
-//     }
-
-//     let [name, alterEgo, abilities] = values;
-//     let id = null;
-
-//     abilities = abilities.split(" ");
-
-//     console.log(
-//       `Our new hero is ${name} aka. ${alterEgo} - with the powers of ${abilities.join(
-//         ", "
-//       )}`
-//     );
-//     const postData = await fetch("https://character-database.becode.xyz/", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({name, shortDescription, image, description }),
-//       });
-const button_Run=document.getElementById("save");
-button_Run.addEventListener("click",imgtodata);
+})
