437b3f017b3744d36c890c40af1c4eca8060be10
Yassine Layachi <yassinelayachi@outlook.com>
2021-08-16 16:39:53 +0200

    displaying list finished

--- list.js
+++ list.js
@@ -5,29 +5,33 @@ const template=async () =>{
     "https://character-database.becode.xyz/characters");
 
   let responseBody = await fetchResponse.json();
+  
 
-  let idResult=localStorage.getItem("vOneLocalStorage"); // multiple variable
-  console.log(idResult);
   
   let target=document.getElementById("target");
+ 
         
   let tpl=document.getElementById("charnew");
   console.log(tpl);
-  let clone=tpl.cloneNode(true).content;
-
-  let hero=responseBody.find(element => element.id==idResult); // object with the chosen id
-  console.log(hero);
-
-  let imageCard=clone.querySelector('.charmain__img');
-  let nameCard=clone.querySelector('.charmain__title');
-  let shortDescriptionCard=clone.querySelector('.charmain__bio');
-
- imageCard.src=`data:image/png;base64,${hero.image}`;
- console.log(imageCard.src);
-  nameCard.innerHTML=hero.name;
- shortDescriptionCard.innerHTML=hero.shortDescription;
   
- target.appendChild(clone);
+ 
+  
+
+    responseBody.forEach(element => {
+      let clone=tpl.cloneNode(true).content;
+      let imageCard=clone.querySelector('.charmain__img');
+      let nameCard=clone.querySelector('.charmain__title');
+      let shortDescriptionCard=clone.querySelector('.charmain__bio');
+    
+     imageCard.src=`data:image/png;base64,${element.image}`;
+     console.log(imageCard.src);
+      nameCard.innerHTML=element.name;
+     shortDescriptionCard.innerHTML=element.shortDescription;
+      
+     target.appendChild(clone);
+    
+    });
+
 
 };
 
