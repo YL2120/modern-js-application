49658b3c052668af994c9755d55a788d62dc6089
Yassine Layachi <yassinelayachi@outlook.com>
2021-08-17 12:29:02 +0200

    delete function added

--- character.js
+++ character.js
@@ -2,9 +2,10 @@ const charImage=document.querySelector(".characterCard__image");
 const charName=document.querySelector(".characterCard__name");
 const charShortDescription=document.querySelector(".characterCard__descriptionShort");
 const charLongDescription=document.querySelector(".characterCard__descriptionLong");
+const charID=localStorage.getItem("charID");
 
 const displayCharacter = async () => {
-    let charID=localStorage.getItem("charID");
+    
     console.log(charID);
     let fetchResponse= await fetch(`https://character-database.becode.xyz/characters/${charID}`);
     let responseBody = await fetchResponse.json();
@@ -16,3 +17,32 @@ const displayCharacter = async () => {
 };
 
 displayCharacter();
+
+
+
+
+document.getElementById("delete").addEventListener("click",async()=>{
+
+    if(confirm("Are you sure you want to delete this character ?")){
+
+
+       let fetchDelete= await fetch(`https://character-database.becode.xyz/characters/${charID}`, {
+     method: 'DELETE',
+     headers:{
+        'Content-Type': 'application/json'
+    }
+    })
+
+    };
+
+    alert("Successful delete. You will be redirected to the main page");
+    setTimeout(
+        ()=> {window.location.replace("index.html")},
+        1000);
+      
+
+
+    
+
+
+});
