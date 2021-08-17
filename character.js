0bfdc0a54cf45e416722c45176c4c1eb1ed7699f
Baysaaaa <bayas.chuluun@gmail.com>
2021-08-17 13:29:32 +0200

    WIP character js and html

--- character.js
+++ character.js
@@ -2,10 +2,11 @@ const charImage=document.querySelector(".characterCard__image");
 const charName=document.querySelector(".characterCard__name");
 const charShortDescription=document.querySelector(".characterCard__descriptionShort");
 const charLongDescription=document.querySelector(".characterCard__descriptionLong");
+const charID=localStorage.getItem("charID");
+const UPDATE_BUTTON=document.getElementById("update");
+const DELETE_BUTTON=document.getElementById("delete");
 
 const displayCharacter = async () => {
-    let charID=localStorage.getItem("charID");
-    console.log(charID);
     let fetchResponse= await fetch(`https://character-database.becode.xyz/characters/${charID}`);
     let responseBody = await fetchResponse.json();
     charImage.src=`data:image/png;base64,${responseBody.image}`;
@@ -14,5 +15,23 @@ const displayCharacter = async () => {
     charLongDescription.innerHTML=responseBody.description;
 
 };
-
 displayCharacter();
+
+const CHAR_FORM=document.getElementById("CHAR_FORM");
+const CHAR_CONTAINER=document.getElementById("CHAR_CONTAINER");
+const TARGET=document.getElementById("TARGET");
+
+CHAR_FORM.style.display="none";
+
+const displayForm = async () => {
+    CHAR_CONTAINER.style.display = "none";
+    CHAR_FORM.style.display = "block";
+    let fetchResponse = await fetch(`https://character-database.becode.xyz/characters/${charID}`);
+    let responseBody = await fetchResponse.json();
+    PREVIEW.style.backgroundImage =`data:image/png;base64,${responseBody.image}`;
+    charName.innerHTML=responseBody.name;
+    charShortDescription.innerHTML=responseBody.shortDescription;
+    charLongDescription.innerHTML=responseBody.description;
+
+}
+UPDATE_BUTTON.addEventListener("click", displayForm);
